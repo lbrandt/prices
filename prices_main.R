ac6544493d110f3c25335282d9069874ec3f8040
Lennart Brandt <lennart@rohde-brandt.de>
2023-03-17 17:21:13 +0000

    Compute price changes by region, shop, item

--- prices_main.R
+++ prices_main.R
@@ -41,12 +41,60 @@ prices_aggregate_by_item %>%
 
 
 # price changes by region, shop, item, time
-
 filter(prices, QDATE == "2023-01-01", item_id == 210102, shop_code == 803)
 
+# Restrict sample
+prices_from2019 = filter(prices, QDATE >= "2019-01-01") %>%
+  select(-c(item_id_raw, indicator_box))
+#rm(prices)
 
-prices_aggregate_by_RSIT = prices %>%
-  group_by(region, shop_code, item_id, QDATE) %>%
-  summarise(price_change = log(price)-log(lag(price, 1)),
+filter(prices_from2019, QDATE == "2023-01-01", item_id == 210102, shop_code == 803)
+
+
+prices_from2019_change = prices_from2019 %>%
+  arrange(item_id, region, shop_code, QDATE) %>%
+  group_by(item_id, region, shop_code) %>%
+  mutate(date_last_quote   = lag(QDATE, 1),
+         months_since_last = round((QDATE - date_last_quote)/30),0) %>%
+  mutate(price_pct      = round(100*((price/lag(price, 1))^(1/months_since_last)-1), 2),
+         indicator_up   = ifelse(price_pct > 0, 1, 0),
+         indicator_down = ifelse(price_pct < 0, 1, 0),
+         indicator_noch = ifelse(price_pct == 0, 1, 0))
+
+
+filter(prices_from2019_change, QDATE == "2022-12-01", item_id == 210102)
+
+
+# collapse region
+prices_from2019_median = prices_from2019 %>%
+  group_by(QDATE, item_id, shop_code) %>%
+  summarise(price_med = median(price),
             nobs = n())
+
+filter(prices_from2019_median, QDATE == "2022-12-01", item_id == 210102)
+
+prices_aggregate_by_SIT = prices_from2019_median %>%
+  arrange(item_id, shop_code) %>%
+  group_by(item_id, shop_code) %>%
+  mutate(price_change   = round(price_med/lag(price_med, 1)-1, 2),
+         indicator_up   = ifelse(price_change>0, 1, 0),
+         indicator_down = ifelse(price_change<0, 1, 0),
+         indicator_noch = ifelse(price_change==0, 1, 0))
+
+
+
+filter(prices_aggregate_by_SIT, QDATE == "2022-12-01", item_id == 210102) %>%
+  ggplot() +
+  geom_histogram(aes(price_change))
+  
+
+
+
+prices_aggregate_by_RSIT = prices_from2019 %>%
+  group_by(region, shop_code, item_id, QDATE) %>%
+  summarise(price_change = price-lag(price, 1),
+            nobs = n())
+
+
+
 
