ff10833e425f1b35a78910a53280f72aeabc68c9
Lennart Brandt <lennart@rohde-brandt.de>
2023-03-18 22:05:25 +0000

    Reproduce RD frequency of monthly price changes
    Script now computes price changes conservatively, computes within-month quantiles, plots time series charts, and kernel density estimates.
    
    For prototyping, restricted sample to 2019 and after.

--- prices_main.R
+++ prices_main.R
@@ -4,6 +4,7 @@
 # -------------------------------------------------------------------------
 
 require(tidyverse)
+require(lubridate)
 require(tictoc)
 
 
@@ -52,18 +53,117 @@ filter(prices_from2019, QDATE == "2023-01-01", item_id == 210102, shop_code == 8
 
 
 prices_from2019_change = prices_from2019 %>%
-  arrange(item_id, region, shop_code, QDATE) %>%
   group_by(item_id, region, shop_code) %>%
+  arrange(QDATE) %>%
   mutate(date_last_quote   = lag(QDATE, 1),
-         months_since_last = round((QDATE - date_last_quote)/30),0) %>%
-  mutate(price_pct      = round(100*((price/lag(price, 1))^(1/months_since_last)-1), 2),
+         months_since_last = round(as.numeric(QDATE - date_last_quote)/30, 0)) %>%
+  mutate(price_last     = lag(price, 1),
+         price_pct      = round(100*((price/price_last)^(1/months_since_last)-1), 2),
          indicator_up   = ifelse(price_pct > 0, 1, 0),
          indicator_down = ifelse(price_pct < 0, 1, 0),
-         indicator_noch = ifelse(price_pct == 0, 1, 0))
+         indicator_noch = ifelse(price_pct == 0, 1, 0)) %>%
+  filter(months_since_last != 0) 
 
 
-filter(prices_from2019_change, QDATE == "2022-12-01", item_id == 210102)
+vvv = filter(prices_from2019_change, QDATE >= "2022-01-01", item_id == 210102)
+#clipr::write_clip(vvv)
 
+
+# Extreme quantiles of unconditional price changes for trimming
+prices_stats = ungroup(prices_from2019_change) %>%
+  filter(months_since_last == 1) %>%
+  summarise(pct_mean  = mean(price_pct),
+            pct_max   = max(price_pct),
+            trim_up   = quantile(price_pct, 0.9995),
+            pct_p95   = quantile(price_pct, 0.95),
+            pct_p80   = quantile(price_pct, 0.80), 
+            pct_p50   = quantile(price_pct, 0.50),
+            pct_p20   = quantile(price_pct, 0.20),
+            pct_p05   = quantile(price_pct, 0.05),
+            trim_down = quantile(price_pct, 1-0.9995),
+            pct_min   = min(price_pct),
+            nobs = n())
+
+
+# Aggregate changes by month
+prices_aggregate_by_month = prices_from2019_change %>%
+  filter(months_since_last == 1) %>%
+  # Trim most extreme 0.1% of price changes
+  filter(price_pct < prices_stats$trim_up, price_pct > prices_stats$trim_down) %>%
+  group_by(QDATE) %>%
+  summarise(share_up   = mean(indicator_up, na.rm = TRUE),
+            share_down = mean(indicator_down, na.rm = TRUE),
+            share_noch = mean(indicator_noch, na.rm = TRUE),
+            # Conditional distribution of price changes
+            pct_up_mean = mean(price_pct[indicator_up==1]),
+            pct_up_p95  = quantile(price_pct[indicator_up==1], 0.95),
+            pct_up_p80  = quantile(price_pct[indicator_up==1], 0.80), 
+            pct_up_p50  = quantile(price_pct[indicator_up==1], 0.50),
+            pct_up_p20  = quantile(price_pct[indicator_up==1], 0.20),
+            pct_up_p05  = quantile(price_pct[indicator_up==1], 0.05),
+            pct_down_mean = mean(price_pct[indicator_down==1]),
+            pct_down_p95  = quantile(price_pct[indicator_down==1], 0.95),
+            pct_down_p80  = quantile(price_pct[indicator_down==1], 0.80), 
+            pct_down_p50  = quantile(price_pct[indicator_down==1], 0.50),
+            pct_down_p20  = quantile(price_pct[indicator_down==1], 0.20),
+            pct_down_p05  = quantile(price_pct[indicator_down==1], 0.05),
+            pct_all_mean = mean(price_pct),
+            pct_all_p95  = quantile(price_pct, 0.95),
+            pct_all_p80  = quantile(price_pct, 0.80), 
+            pct_all_p50  = quantile(price_pct, 0.50),
+            pct_all_p20  = quantile(price_pct, 0.20),
+            pct_all_p05  = quantile(price_pct, 0.05),
+            nobs = n())
+
+#clipr::write_clip(prices_aggregate_by_month)
+
+### Charts
+# Plot shares
+ggplot(prices_aggregate_by_month) +
+  geom_line(aes(x = QDATE, y = share_up, color = "Share of prices rising within the month")) +
+  geom_line(aes(x = QDATE, y = share_down, color = "Share of prices falling within the month")) +
+  geom_line(aes(x = QDATE, y = share_noch, color = "Share of prices unchanged"))
+
+# Plot unconditional quantiles over time
+ggplot(prices_aggregate_by_month) +
+  geom_line(aes(x = QDATE, y = pct_all_mean, color = "Average price change")) +
+  geom_line(aes(x = QDATE, y = pct_all_p95, color = "95th percentile")) +
+  geom_line(aes(x = QDATE, y = pct_all_p50, color = "Median")) +
+  geom_line(aes(x = QDATE, y = pct_all_p05, color = "5th percentile"))
+
+# Plot quantiles of price increases
+ggplot(prices_aggregate_by_month) +
+  geom_line(aes(x = QDATE, y = pct_up_mean, color = "Average price increase")) +
+  geom_line(aes(x = QDATE, y = pct_up_p80, color = "80th percentile")) +
+  geom_line(aes(x = QDATE, y = pct_up_p50, color = "Median")) +
+  geom_line(aes(x = QDATE, y = pct_up_p20, color = "20th percentile"))
+
+# Plot quantiles of price decreases
+ggplot(prices_aggregate_by_month) +
+  geom_line(aes(x = QDATE, y = pct_down_mean, color = "Average price decrease")) +
+  geom_line(aes(x = QDATE, y = pct_down_p80, color = "80th percentile")) +
+  geom_line(aes(x = QDATE, y = pct_down_p50, color = "Median")) +
+  geom_line(aes(x = QDATE, y = pct_down_p20, color = "20th percentile"))
+
+
+# Plot kernel densities
+uuu = prices_from2019_change %>%
+  filter(months_since_last == 1) %>%
+  # Trim most extreme 0.1% of price changes
+  filter(price_pct < prices_stats$trim_up, price_pct > prices_stats$trim_down) %>%
+  filter(QDATE == "2022-10-01") 
+
+summary(uuu)
+
+uuu %>% filter(indicator_up == 1) %>%
+  ggplot() +
+  geom_density(aes(x = price_pct)) +
+  xlim(0, 120)
+  
+uuu %>% filter(indicator_down == 1) %>%
+  ggplot() +
+  geom_density(aes(x = price_pct)) +
+  xlim(-100, 0)
 
 # collapse region
 prices_from2019_median = prices_from2019 %>%
